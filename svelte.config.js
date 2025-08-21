afe54f3bf99e87c8dbb1b11e98cbaea7120c7b66
codeintheshell <thibault-castelli@users.noreply.github.com>
2025-08-21 16:08:58 +0200

    Setup vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,27 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { BYPASS_TOKEN } from '$env/static/private';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	isr: {
+		expiration: 60,
+		bypassToken: BYPASS_TOKEN,
+		allowQuery: ['search']
+	},
+	kit: {
+		adapter: adapter({
+			images: {
+				sizes: [640, 828, 1200, 1920],
+				formats: ['image/avif', 'image/webp'],
+				minimumCacheTTL: 300,
+				domains: ['example-app.vercel.app']
+			}
+		})
+	}
 };
 
 export default config;
