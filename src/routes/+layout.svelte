3a74ebb2df020a5df4c28756bae179d8c818dcb7
codeintheshell <thibault-castelli@users.noreply.github.com>
2025-08-22 10:59:59 +0200

    Add hamburger menu for smartphones

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,18 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import NavLink from '$lib/components/NavLink.svelte';
-	import { Linkedin, Github } from '@lucide/svelte';
+	import { Linkedin, Github, Menu } from '@lucide/svelte';
 	import { Button } from '$lib/components/ui/button';
+	import * as Menubar from '$lib/components/ui/menubar/index.js';
+	import { goto } from '$app/navigation';
 
 	let { children } = $props();
 
 	const links = [
 		{ href: '/', text: 'Home' },
+		{ href: '/experience', text: 'Experience' },
 		{ href: '/projects', text: 'Projects' },
 		{ href: '/skills', text: 'Skills' },
-		{ href: '/experience', text: 'Experience' },
 		{ href: '/education', text: 'Education' },
 		{ href: '/contact', text: 'Contact' }
 	];
@@ -23,7 +25,7 @@
 </svelte:head>
 
 <header
-	class="sticky top-0 z-10 items-center justify-between space-y-6 border-b border-b-primary bg-background px-6 py-4 shadow-2xl xl:flex"
+	class="sticky top-0 z-10 flex items-center justify-between space-y-6 border-b border-b-primary bg-background px-6 py-4 shadow-2xl md:block xl:flex"
 >
 	<a href="/" class="mb-0">
 		<h1 class="mb-0 text-3xl">
@@ -33,16 +35,33 @@
 		</h1>
 	</a>
 
-	<nav class="mt-5 xl:mt-0">
-		<span class="text-muted-foreground">$pages = [</span>
-		{#each links as link, i (link.href)}
-			<NavLink href={link.href} text={link.text} />{#if i === links.length - 1}{:else}<span
-					class="text-muted-foreground">,&nbsp;</span
-				>
-			{/if}
-		{/each}
-		<span class="text-muted-foreground">];</span>
+	<nav class="mt-5 mb-0 xl:mt-0">
+		<div class="hidden md:block">
+			<span class="text-muted-foreground">$pages = [</span>
+			{#each links as link, i (link.href)}
+				<NavLink href={link.href} text={link.text} />{#if i === links.length - 1}{:else}<span
+						class="text-muted-foreground">,&nbsp;</span
+					>
+				{/if}
+			{/each}
+			<span class="text-muted-foreground">];</span>
+		</div>
 	</nav>
+
+	<div class="md:hidden">
+		<Menubar.Root>
+			<Menubar.Menu>
+				<Menubar.Trigger><Menu /></Menubar.Trigger>
+				<Menubar.Content>
+					{#each links as link (link.href)}
+						<Menubar.Item onclick={() => goto(link.href)}
+							><NavLink href={link.href} text={link.text} /></Menubar.Item
+						>
+					{/each}
+				</Menubar.Content>
+			</Menubar.Menu>
+		</Menubar.Root>
+	</div>
 </header>
 
 <main class="container mx-auto grow p-6">
